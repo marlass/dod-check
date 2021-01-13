bc30b0c8eda31decd0bec7bef8eb628711f134df
Marcin Lasak <lasakmarcin90@gmail.com>
2021-01-13 20:09:58 +0100

    chore: Check 2 things

--- .github/dod-check/src/index.ts
+++ .github/dod-check/src/index.ts
@@ -2,7 +2,12 @@ import * as github from '@actions/github';
 
 async function run() {
   const context = github.context;
-  console.log(context);
+  console.log(
+    context.payload.comment,
+    context.actor,
+    context.payload.issue?.user,
+    context.payload.issue?.pull_request
+  );
 }
 
 run();
